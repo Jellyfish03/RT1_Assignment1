768d7cdfe07bc86803065488f2144d4224a3d2ac
Jellyfish03 <131369162+Jellyfish03@users.noreply.github.com>
2023-04-22 12:24:47 +0200

    Update assignment.py

--- Assignment1_RT1/assignment.py
+++ Assignment1_RT1/assignment.py
@@ -6,8 +6,8 @@ from sr.robot import *
 a_th = 2.0 #float: Threshold for the control of the orientation
 d_th = 0.4 #float: Threshold for the control of the linear distance
 
-fin_silver = [] #record the number of silver token that is done
-fin_gold = [] #record the number of gold token that is done
+fin_silver = [] #record the number of found silver token 
+fin_gold = [] #record the number of found gold tokens
 
 R = Robot() #instance of the class Robot
 
@@ -37,85 +37,85 @@ def turn(speed, seconds):
 
 def find_token(color): 
     """
-    This function is for fiding the closest token considering its color
+    This function is for finding the closest token considering its color
 
     Arg:    color: true=silver false=gold
 
     Returns:dist (float): distance of the closest token (-1 if no token is detected)
-	        rot_y (float): angle between the robot and the token (-1 if no token is detected)
+           rot_y (float): angle between the robot and the token (-1 if no token is detected)
+           num = offset
     """
     dist=100
     for token in R.see():
         if color == True and token.dist < dist and token.info.marker_type is MARKER_TOKEN_SILVER: #silver and closer than previous token
-            if token.info.offset in fin_silver: #ignore tokens which are already delivered
+            if token.info.offset in found_silver: #ignore tokens which are already been delivered
                 continue
             else:
                 dist=token.dist
                 rot_y=token.rot_y
                 num=token.info.offset #to distinguish token if it's delivered or not
         elif color == False and token.dist < dist and token.info.marker_type is MARKER_TOKEN_GOLD:
-            if token.info.offset in fin_gold: #ignore tokens which are already delivered
+            if token.info.offset in found_gold: #ignore tokens which are already delivered
                 continue
             else:
                 dist=token.dist
                 rot_y=token.rot_y
-                num=token.info.offset #to distinguish token if it's delivered or not
+                num=token.info.offset
     if dist==100:
         return -1, -1, -1
     else:
-        return dist, rot_y, num # distance, angle, offset (next target)
+        return dist, rot_y, num
 
-def record(color, num):
+def rem(color, num):
+
     """
-    This function is for record the offset of tokens which the robot has already reached
-
     Args:   color:  silver=true, gold=false
-            num:    the number given by find_token, which indicates the offset of token
+            num:    offset of the token
     """
-    if color == True: #count silvers
-        fin_silver.append(num)
-        print("REACHED SILVER: "+str(fin_silver))
-    elif color == False: #count golds
-        fin_gold.append(num)
-        print("REACHED GOLD: "+str(fin_gold))
-
+    if  color == False: #counts golds
+        found_gold.append(num)
+        print("REACHED GOLD: "+str(found_gold))
+    elif   
+        color == True: #counts silvers
+        found_silver.append(num)
+        print("REACHED SILVER: "+str(found_silver))
 
 def main():
     print("MISSION STARTED!") 
     silver = True #look for silver at first
     while 1:
         dist, rot_y, num = find_token(silver) #info about closest token(silver/gold)
-        if dist==-1: #when find_token couldn't find any token
-            print("NO TOKEN HAS FOUND YET")
+        if dist==-1: 
+            print("NO TOKEN FOUND (YET)")
             turn(10,1)
         elif dist <d_th + 0.2: #if the robot is about to reach token, we grab/release it
-            print("THERE IS")
+            print("Found You!")
             if silver == True:
-                forward(20, 1) #go forward a little bit to grab
-                print("I GOT YOU")
+                forward(20, 1) #go forward a bit
+                print("Got you!")
                 R.grab() #grab silver token
-                record(silver, num) #after grab it, record its number
+                rem(silver, num) #recording its number
                 turn(-20, 2)
-                print("DELIVER TIME!")
+                print("Time to deliver!")
             elif silver == False:
-                print("HERE YOU ARE!")
+                print("Here you are!")
                 R.release()
-                record(silver, num)
-                if len(fin_gold) == 6:
+                rem(silver, num)
+                if len(found_gold) == 6:
                     forward(-20, 2)
                     turn(+20, 1.5)
-                    print("MISSION COMPLETED!")
+                    print("GJ!MISSION COMPLETED!")
                     exit()
                 turn(+20, 2)
             silver = not silver #switch silver/gold
-        elif -a_th<= rot_y <= a_th: # if the robot is well aligned with the token, we go forward
-            print("KEEP THIS WAY")
+        elif -a_th<= rot_y <= a_th: 
+            print("Good direction")
             forward(40, 0.5)
-        elif rot_y < -a_th: # if the robot is not well aligned with the token, we move it on the left or on the right
-            print("LEFT")
-            turn(-4, 0.25)
         elif rot_y > a_th:
-            print("RIGHT")
+            print("right")
             turn(+4, 0.25)
-
+        elif rot_y < -a_th: 
+            print("left")
+            turn(-4, 0.25)
+		
 main()
